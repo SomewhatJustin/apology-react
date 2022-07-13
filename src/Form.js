43141f40f0944d049c311901addeb03d7a6b9b3b
Justin Fowler <justin.fowler@hey.com>
2022-07-13 14:54:51 -0500

    Show a warning if approaching 100 chars.

--- src/Form.js
+++ src/Form.js
@@ -5,16 +5,35 @@ import { getApology } from './utils'
 export default function Form(props) {
 
   const inputEl = useRef(null)
+  const errorEl = useRef(null)
 
   useEffect(() => {
     inputEl.current.focus()
   }, [])
 
+  useEffect(() => {
+    let currentLength = checkInputLength()
+
+    if (currentLength < 70) {
+      inputEl.current.className = ""
+      console.log("is fine")
+    } else if (currentLength >= 70 && currentLength < 100) {
+      inputEl.current.className = "warning"
+      console.log("is warning")
+    } else if (currentLength >= 100) {
+      inputEl.current.className = "error"
+      console.log("is error")
+    }
+  }, [props.myCrime])
+
   const handleSubmit = (event) => {
-    props.setIsLoading(true)
     event.preventDefault()
-    getResponse()
-    // console.log(event.target[0].value)
+    if (checkInputLength() > 100) {
+      errorEl.current.className = ""
+    } else {
+      props.setIsLoading(true)
+      getResponse()
+    }
   }
 
   async function getResponse() {
@@ -29,11 +48,16 @@ export default function Form(props) {
     props.setMyCrime(event.target.value)
   }
 
+  const checkInputLength = () => {
+    return props.myCrime.length
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="my-sin"></label>
       <input type="text" name="my-sin" onChange={handleChange} value={props.myCrime} ref={inputEl}></input>
       <input type="submit" value="Send in my confession" />
+      <p ref={errorEl} className="hidden">Your confession must be less than 100 characters. You're at {checkInputLength()}</p>
     </form>
   )
 }
